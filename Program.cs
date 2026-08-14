97b6f4c0c4162bb9fe9ae0e295162b50236e1134
Adrian Ariel <pedaeadr@gmail.com>
2026-08-13 22:20:08 -0300

    feat(PROGRMA):  jwt in program

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
 using Microsoft.EntityFrameworkCore;
 using BankDb;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 //ADD BANK
 builder.Services.AddDbContext<Bank>();
-builder.Services.AddJwtAuth();
+//JWT 
+var key = Encoding.ASCII.GetBytes("ASDASDASDAD1312DA2131JE12H3OI12JP");
+builder.Services.AddAuthentication(options =>
+{
+options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+options.TokenValidationParameters = new TokenValidationParameters
+{
+ValidateIssuerSigningKey = true,
+IssuerSigningKey = new SymmetricSecurityKey(key),
+ValidateIssuer = false,
+ValidateAudience = false
+};
+});
+
+
+
+
 var app = builder.Build();
-app.MapControllers();
 app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthorization(); 
+app.MapControllers();
 
 app.Run();
 
